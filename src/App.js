e5308abc99f523e5228afc7f8a5ea07deaf49ebd
tush-tr <tusharrajput604@gmail.com>
2021-11-14 15:32:43 +0530

    first edit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Tushar Rajpoot</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
