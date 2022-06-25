d6f147e642312d8fc3f7bdc5cd50c37f06e2b83d
tush-tr <tusharrajput604@gmail.com>
2022-06-25 06:44:13 +0530

    new code challenge added

--- src/components/HomePage/CodeChallenges/challengeWrapper.js
+++ src/components/HomePage/CodeChallenges/challengeWrapper.js
@@ -2,15 +2,23 @@ import styled from "styled-components";
 const ChallengeWrapper = styled.div`
     text-decoration: none;
     color: white;
-    width: 40%;
+    width: 50%;
     background-color: #09244c;
     padding: 20px;
+    border-radius: 5px;
+    margin: 20px;
+    box-shadow: -2px 1px 2px 0px #64ffda;
+    a{
+        text-decoration: none;
+        color:#64ffda;
+    }
     & h3{
         background-color: #0b182c;
         padding: 5px;
         padding-left: 10px;
         border-radius: 10px;
         font-size: 1.5rem;
+        box-shadow: 1px 1px 2px 0px;
     }
     & p{
         font-size: 0.8rem;
--- src/components/HomePage/CodeChallenges/challenges.jsx
+++ src/components/HomePage/CodeChallenges/challenges.jsx
@@ -1,14 +1,21 @@
 import Challenge from "./challenge";
+import CodeChallenges from "./CodeChallenge";
 const Challenges = () => {
   return (
-    <div className="code-challenges">
-      <div className="row-1">
+    <CodeChallenges>
+      <div className="code-challenge-row row-1">
         <Challenge
           challengeName="Hamming Distance"
+          link="https://tush-tr.github.io/vanilla-js-projects/coding-challenges/hamming-distance/"
           description="The Hamming distance between two integers is the number of positions at which the corresponding bits are different."
         />
+        <Challenge
+          challengeName="Buy and Sell Stock"
+          link="https://tush-tr.github.io/vanilla-js-projects/coding-challenges/buysell-stock/"
+          description="maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock."
+        />
       </div>
-    </div>
+    </CodeChallenges>
   );
 };
 export default Challenges;
